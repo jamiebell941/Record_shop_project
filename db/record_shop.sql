99fd47cec800b1f615a925d8fb54c9f39beeea57
jamiebell941 <jamiebell941@gmail.com>
2020-03-13 15:02:41 +0000

    added tables in sql file

---
+++ db/record_shop.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS records;
+DROP TABLE IF EXISTS artists;
+
+CREATE TABLE artists(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  activity VARCHAR(255)
+);
+
+CREATE TABLE records(
+  id SERIAL PRIMARY KEY,
+  artist_id INT REFERENCES artists(id) ON DELETE CASCADE,
+  title VARCHAR(255),
+  genre VARCHAR(255),
+  released INT,
+  artwork VARCHAR(255),
+  quantity INT,
+  buy_price INT,
+  sell_price INT,
+);
