76a28fb54eae447a54d0a9f635a098f0a403d07d
jamiebell941 <jamiebell941@gmail.com>
2020-03-17 09:20:18 +0000

    most functionaility for records working, artist issue still not fixed

--- record_controller.rb
+++ record_controller.rb
@@ -14,17 +14,23 @@ get '/records' do
   erb(:inventory)
 end
 
-get '/records/new' do # new
+get '/records/new' do
   erb(:new)
 end
 
-post '/records' do # create
+post '/records' do
   @record = Record.new( params )
   @record.save()
   erb( :create )
 end
 
-get '/records/:id/edit' do # edit
+get '/records/:id/edit' do
   @record = Record.find_id( params[:id] )
   erb( :edit )
+end
+
+post '/records/:id/delete' do
+  record = Record.find_id( params[:id] )
+  record.delete()
+  redirect to '/records'
 end
